c0182f3119712caa543ba2debb15a3697485ec15
danielorkingit <danielorkin@icloud.com>
2022-11-20 16:44:44 +0100

    last

--- src/bwinf41_fahrradwerkstatt/Main.java
+++ src/bwinf41_fahrradwerkstatt/Main.java
@@ -1,8 +1,5 @@
 package bwinf41_fahrradwerkstatt;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Scanner;
